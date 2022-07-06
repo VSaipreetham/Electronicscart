0627ba3d1adb929553c7b8b08e62b43861a0361b
VSaipreetham <www.saipreetham@gmail.com>
2022-07-06 11:30:16 +0530

    first commit

---
+++ ecommerce/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Designed by Sai preetham <a href="https://github.com/VSaipreetham">@2020 SE project</a></strong>
+</footer>
